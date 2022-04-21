a87af8606464ca6d2d97c77efca12cf495ac8b00
DarkRastafar <mvmsurgutmebel@gmail.com>
2022-04-21 22:46:35 +0500

    initial commit

---
+++ db/base.py
@@ -0,0 +1,14 @@
+from databases import Database
+from sqlalchemy import create_engine, MetaData
+
+from core.config import DATABASE_URL
+
+
+engine = create_engine(DATABASE_URL)
+database = Database(DATABASE_URL)
+metadata = MetaData()
+
+
+
+
+
