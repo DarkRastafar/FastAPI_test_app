3335c27dd0e0a8c00b43535b4976f379d46938ce
DarkRastafar <mvmsurgutmebel@gmail.com>
2022-05-05 15:49:13 +0500

    "add movies"

--- db/base.py
+++ db/base.py
@@ -1,14 +1,56 @@
 from databases import Database
-from sqlalchemy import create_engine, MetaData
-
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Boolean
 from core.config import DATABASE_URL
 
+from pydantic import BaseModel
 
-engine = create_engine(DATABASE_URL)
+
 database = Database(DATABASE_URL)
 metadata = MetaData()
 
 
+notes = Table(
+    "notes",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("text", String),
+    Column("completed", Boolean),
+)
 
 
+movies = Table(
+    "movies",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("title", String),
+    Column("description", String)
+)
+
+
+engine = create_engine(
+    DATABASE_URL
+)
+metadata.create_all(engine)
+
+
+class NoteIn(BaseModel):
+    text: str
+    completed: bool
+
+
+class Note(BaseModel):
+    id: int
+    text: str
+    completed: bool
+
+
+class Movies(BaseModel):
+    id: int
+    title: str
+    description: str
+
+
+class MoviesIn(BaseModel):
+    title: str
+    description: str
 
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
-
-from db.base import database
-
+from typing import List
+from db.base import database, notes, NoteIn, Note, movies, Movies, MoviesIn
 
 app = FastAPI()
 
@@ -20,6 +19,26 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
+
+
+@app.get("/notes/", response_model=List[Note])
+async def read_notes():
+    query = notes.select()
+    return await database.fetch_all(query)
+
+
+@app.post("/notes/", response_model=Note)
+async def create_note(note: NoteIn):
+    query = notes.insert().values(text=note.text, completed=note.completed)
+    last_record_id = await database.execute(query)
+    return {**note.dict(), "id": last_record_id}
+
+
+@app.post("/movies/", response_model=Movies)
+async def create_movie(movie: MoviesIn):
+    query = movies.insert().values(title=movie.title, description=movie.description)
+    last_record_id = await database.execute(query)
+    return {**movie.dict(), "id": last_record_id}
 
 
 if __name__ == '__main__':
